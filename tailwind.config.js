0340ced955106318b5f0dbd2edfd3a6dc3503cc9
thebuilderekes <ekeopreberedugo1@gmail.com>
2023-11-21 23:39:02 +0100

    updated link to solve vercel build issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  mode: "jit",
-  content: [
-    "./src/**/*.{html,js}",
-    "./components/**/*.{html,js}",
-    "./pages/**/*.{html,js}",
-  ],
-  theme: {
-    screens: {
-      xs: "320px",
-      // => @media (min-width: 640px) { ... }
-    },
-  },
+	mode: "jit",
+	content: [
+		"./index.html",
+		"./src/**/*.{html,js}",
+		"./components/**/*.{html,js}",
+		"./pages/**/*.{html,js}",
+	],
+	theme: {
+		screens: {
+			xs: "320px",
+			// => @media (min-width: 640px) { ... }
+		},
+	},
 };
